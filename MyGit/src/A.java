79b270fd4cdd462e6bd12d2da55b8244cc8cf78b
김진우 <@DESKTOP-IL1TKEJ>
2021-06-13 13:02:54 +0900

    A.java 변경

--- MyGit/src/A.java
+++ MyGit/src/A.java
@@ -4,6 +4,7 @@ public class A {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is A");
+		System.out.println("This is A 2nd version");
 	}
 
 }
